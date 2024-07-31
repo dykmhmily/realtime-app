255daaa06705fe1c7aac5150faa07c236a851a96
cherili <406180771@qq.com>
2024-07-31 23:26:38 +0800

    frontend: polling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,73 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import Card from './components/card'
 import { fetchAssetsList, Asset } from './utils/http'
 
-function App() {
-  const [list, setList] = useState<Array<Asset>>([])
+const useAnimationFrame = (callback) => {
+  const frameRef = useRef(0)
+  const retryCountRef = useRef(0)
+  const maxRetries = 5
+  const retryDelay = 2000 // ms
 
-  const getList = async () => {
+  const animate = async () => {
     try {
-      const res = await fetchAssetsList()
-      setList(res)
+      await callback()
+      retryCountRef.current = 0 // reset
+      frameRef.current = requestAnimationFrame(animate)
     } catch (error) {
       console.error(error)
+      retryCountRef.current++
+      if (retryCountRef.current <= maxRetries) {
+        const delay = retryDelay * Math.pow(2, retryCountRef.current)
+        console.log(`Retrying in ${delay}ms...`)
+        setTimeout(() => {
+          frameRef.current = requestAnimationFrame(animate)
+        }, delay)
+        return
+      } else {
+        console.error('Max retries reached. Stopping retries.')
+        return
+      }
     }
   }
 
   useEffect(() => {
-    getList()
+    frameRef.current = requestAnimationFrame(animate)
+    return () => cancelAnimationFrame(frameRef.current)
   }, [])
+}
+
+function App() {
+  const [list, setList] = useState<Array<Asset>>([])
+  const [error, setError] = useState<string | null>(null)
+
+  useAnimationFrame(async () => {
+    try {
+      const res: Array<Asset> = await fetchAssetsList()
+      setList(res)
+      setError(null)
+    } catch (err) {
+      setError('Failed. Please try again later....')
+      throw err
+    }
+  })
 
   return (
     <div className="container p-10">
       <h1 className="container text-xl font-semibold">Cryptocurrency Realtime Price</h1>
       <div className="container flex flex-row flex-wrap w-200 mt-8">
-        {list.map((item) => {
-          return (
-            <div className="basis-1/3 pr-4 mb-4" key={item.id}>
-              <Card key={item.id} currency={item.currency} price={item.price} volume={item.volume}></Card>
-            </div>
-          )
-        })}
+        {error
+          ? error
+          : list.map((item) => {
+              return (
+                <div className="basis-1/3 pr-4 mb-4" key={item.id}>
+                  <Card key={item.id} currency={item.currency} price={item.price} volume={item.volume}></Card>
+                </div>
+              )
+            })}
       </div>
+      {/* <div>
+        <Pagination></Pagination>
+      </div> */}
     </div>
   )
 }
